02c4dff4699b2e8f36d0f5b39c652adc955ae07b
GauravAccGit <eng.gupta26@gmail.com>
2017-08-06 12:31:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 //Change the text of main-textdiv
 var element = document.getElementById('main-text');
 element.innerHTML = 'New Value';
+
+var imgName = document.getElementById('img');
+
+imgName.onclick = function() {
+  imgName.style.marginLeft = '100px';  
+};
