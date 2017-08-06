3027f5f4f3f776af2502d49175165c4e4cac1f51
GauravAccGit <eng.gupta26@gmail.com>
2017-08-06 12:36:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,14 @@ element.innerHTML = 'New Value';
 
 var imgName = document.getElementById('madi');
 
+var marginLeft = 0;
+
+function moveRight() {
+    marginLeft = marginLeft + 10;
+    imgName.style.marginLeft = marginLeft + 'px';
+}
+
 imgName.onclick = function() {
-  imgName.style.marginLeft = '100px';  
+  var interval = setInterval(moveRight,100);
+  //imgName.style.marginLeft = '100px';  
 };
