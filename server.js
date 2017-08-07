b9fa1231be626fdc565e084d6eb6c6e098cd807f
GauravAccGit <eng.gupta26@gmail.com>
2017-08-07 10:45:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -84,6 +84,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var counter = 0;
+app.get('/counter', function(req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
+});
+
 app.get('/:articleName', function (req, res) {
   //This :/ is a facility given by express framework to extract the 'part of path' as a variable from URL
   //such that it can be indexed directly into an array of strings!!
