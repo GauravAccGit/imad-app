fe32c12918e264f03ca47b28ce466aa6dda999a1
GauravAccGit <eng.gupta26@gmail.com>
2017-08-07 11:01:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,19 +4,33 @@ console.log('Loaded!');
 //var element = document.getElementById('main-text');
 //element.innerHTML = 'New Value';
 
-var imgName = document.getElementById('madi');
+//var imgName = document.getElementById('madi');
 
-var marginLeft = 0;
+//var marginLeft = 0;
 
-function moveRight() {
-    marginLeft = marginLeft + 1;
-    imgName.style.marginLeft = marginLeft + 'px';
-}
+//function moveRight() {
+//    marginLeft = marginLeft + 1;
+//    imgName.style.marginLeft = marginLeft + 'px';
+//}
 
-imgName.onclick = function() {
+//imgName.onclick = function() {
   //var count = 0;
   //for(count = 0; count < 50; count++) {
-      var interval = setInterval(moveRight,50);
+//      var interval = setInterval(moveRight,50);
       //imgName.style.marginLeft = '100px';
   //}
+//};
+
+var button = document.getElementById("counter");
+
+button.onclick = function() {
+  // Send a request to counter endoint
+  
+  //Capture the response and store it in a variable
+  
+  //Render the response in correct span
+  counter = counter + 1;
+  var span = document.getElementById("span");
+  span.innerHTML = counter.toString();
+  
 };
