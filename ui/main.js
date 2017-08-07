19e0b1671e831114d15f7927ea35e45a0c6b740d
GauravAccGit <eng.gupta26@gmail.com>
2017-08-07 11:43:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,6 @@
 console.log('Loaded!');
 
+//Counter Code
 var button = document.getElementById("counter");
 
 button.onclick = function() {
@@ -25,6 +26,30 @@ button.onclick = function() {
   request.open('GET', 'http://enggupta26.imad.hasura-app.io/counter', true);
   request.send(null);
   
+};
+
+//Submit name
+var nameInput = document.getElementById('name');
+var name = nameInput.value;
+
+var submit = document.getElementById('submit_btn');
+
+submit.onclick = function() {
+    
+    // Create a request to name endoint
+    
+      
+  
+  var names = ['name1', 'name2', 'name3'];
+  var list = '';
+  for(var i=0; i < names.length; i++) {
+      list += '<li>' + names[i] + '</li>';
+  }
+  
+  var ul = document.getElementById('namelist');
+  ul.innerHTML = list;
+  
+    
 };
 
 //Change the text of main-textdiv
